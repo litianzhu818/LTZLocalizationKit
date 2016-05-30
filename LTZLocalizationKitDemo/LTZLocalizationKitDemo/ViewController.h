a6a53ebd7fb46539755a23967756338c3fd86dca
Peter Lee <peter.lee@afusion.com>
2016-05-30 18:19:08 +0800

    add pod file and test demo project

---
+++ LTZLocalizationKitDemo/LTZLocalizationKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LTZLocalizationKitDemo
+//
+//  Created by Peter Lee on 16/5/30.
+//  Copyright © 2016年 Peter Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
